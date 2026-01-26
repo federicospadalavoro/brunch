bffbb2462bdcbdbbd178d994747ed1754012b1fb
Federico <Federico@Mac-mini-di-F.local>
2026-01-26 16:50:53 +0100

    chore: setup github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base necessario per GitHub Pages (nome repo)
+  base: '/brunch/',
   plugins: [react()],
 })
